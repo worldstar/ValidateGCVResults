41c53fd7c3019ddb2f685f4197041f7763fed7f9
s74101234 <s74101234@gmail.com>
2017-04-10 11:21:25 +0800

    Updata TimeSchedule
    Updata TimeSchedule 4/10

--- TimeSchedule/src/timeschedule/TimeSchedule.java
+++ TimeSchedule/src/timeschedule/TimeSchedule.java
@@ -13,49 +13,72 @@ import java.util.*;
  */
 public class TimeSchedule {
 
+    private int completionTime = 0;
+    private ArrayList<Integer> Sequence;
+    private ArrayList<Integer> processingTime;
+    private ArrayList<Integer> releaseDate;
+
+//  private double [] r;       //  release date.
+//  private double [] p;       //  processing time
+//  private double [] d;       //  due-date
+//  private double [] d_bar;   //  deadline
+//  private double [] e;       //  revenue
+//  private double [] w;       //  weight
+//  private double [][] s;     //  setup times
+//  private int size;          //  instance lengh
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //create list,pi,ri
-        ArrayList<Integer> list = new ArrayList(Arrays.asList(5,3,1,2,4));
-        ArrayList<Integer> pi = new ArrayList(Arrays.asList(2,4,8,6,10));
-        ArrayList<Integer> ri = new ArrayList(Arrays.asList(1,3,7,5,9));
-
-        int result = 0;
-        TimeSchedule ts = new TimeSchedule();
-
-        //result
-        result = ts.Sequence(list, pi, ri);
-        System.out.println(result);
-
+    public void setSequence(ArrayList<Integer> Sequence) {
+        this.Sequence = Sequence;
     }
 
-    private static int Sequence(ArrayList<Integer> list, ArrayList<Integer> pi, ArrayList<Integer> ri) {
-        int sum = 0;
+    public void setProcessingTime(ArrayList<Integer> processingTime) {
+        this.processingTime = processingTime;
+    }
 
-        //print startlist
+    public void setReleaseDate(ArrayList<Integer> releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public static void main(String[] args) {
+        //create Sequence,processingTime,ri
+        TimeSchedule ts = new TimeSchedule();
+        ts.setSequence(new ArrayList(Arrays.asList(1, 2, 3, 4, 5)));
+        ts.setReleaseDate(new ArrayList(Arrays.asList(1, 3, 7, 5, 9))); //Goods arrived Date.
+        ts.setProcessingTime(new ArrayList(Arrays.asList(2, 4, 8, 6, 10)));
+
+        //result
+        ts.Sorting();
+    }
+
+    private int Sorting() {
+
+        //print startSequence
         System.out.print("Sequence : " + "\t");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i) + "\t");
+        for (int i = 0; i < Sequence.size(); i++) {
+            System.out.print(Sequence.get(i) + "\t");
         }
         System.out.println();
 
         //Processing the completion time
         System.out.print("Completion: " + "\t");
 
-        for (int i = 0; i < list.size(); i++) {
-            if (sum > ri.get(list.get(i) - 1)) {
-                sum += pi.get(list.get(i) - 1);
-            } else {
-                sum += pi.get(list.get(i) - 1) + (ri.get(list.get(i) - 1) - sum);
+        for (int i = 0; i < Sequence.size(); i++) {
+            int index = Sequence.get(i) - 1;
+            if (completionTime > releaseDate.get(index)) 
+            {
+                completionTime += processingTime.get(index);
+            } 
+            else 
+            {
+                completionTime += processingTime.get(index) + (releaseDate.get(index) - completionTime);
             }
-            
-            System.out.print(sum + "\t");
+
+            System.out.print(completionTime + "\t");
         }
 
-        return sum;
+        return completionTime;
     }
 
 }
