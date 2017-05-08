f1519d574ce7be8e0318db55df6f7f7e685651fe
s74101234 <s74101234@gmail.com>
2017-05-08 11:48:39 +0800

    Revise Readtxt
    add a custom range of  function.
    add get function.

--- Readtxt/Readtxt.java
+++ Readtxt/Readtxt.java
@@ -10,18 +10,29 @@ import java.io.*;
  * @author Guo Yu-Cheng
  */
 public class Readtxt {
-  private String fileName;
-  private String[] Number;  
-  private int tempcompletionTime = 0;
-  private int[] completionTime;
-  private int[] Sequence;
-  private int[] processingTime;
-  private int[] releaseDate;
-  private int[] dueDate;
-  private int[] deadline;
-  private double[] revenue;
+    private String fileName;
+    private String[] Number;
+    private String[] STxt;
+    private final int NumberOfColumns = 4; 
+    private static int TxtLength ; //Read TotalLength of the Txt 
+    private int ReadTxtSize; //Current Length of the Txt
+    private int[] completionTime;
+    private int[] Sequence;
+    private int[] processingTime;
+    private int[] releaseDate;
+    private int[] dueDate;
+    private int[] deadline;
+    private double[] revenue;
   
-  public void setcompletionTime(int[] completionTime) {
+    public void setTxtLength(int TxtLength) {
+        this.TxtLength = TxtLength;
+    }
+    
+    public void setReadTxtSize(int ReadTxtSize) {
+        this.ReadTxtSize = ReadTxtSize;
+    }
+  
+    public void setcompletionTime(int[] completionTime) {
         this.completionTime = completionTime;
     }
 
@@ -37,8 +48,8 @@ public class Readtxt {
         this.releaseDate = releaseDate;
     }
 
-    public void setDueDate(int[] setdueDate) {
-        this.dueDate = setdueDate;
+    public void setDueDate(int[] dueDate) {
+        this.dueDate = dueDate;
     }
 
     public void setDeadline(int[] deadline) {
@@ -49,7 +60,7 @@ public class Readtxt {
         this.revenue = revenue;
     }
     
-    public void setTxtData(String fileName)
+    public void setReadTxtData(String fileName)
     {
       this.fileName = fileName;
     }
@@ -63,43 +74,98 @@ public class Readtxt {
             while ((eachLine = br.readLine()) != null) {
                 TxtAll += eachLine + "\n";
             }                     
-            String[] STxt = TxtAll.split("\t|\n");
+            STxt = TxtAll.split("\t|\n");
             
-            //Set Size
-            int Numberindex = 4,due_dayindex = 5,processingTimeindex = 7;
-            int STxtlength = (STxt.length / 4)-1;
-            Number = new String[STxtlength];
-            processingTime = new int[STxtlength];
-            dueDate = new int[STxtlength];
-            
-            //save value of Txt
-            for (int i = 0; i < STxtlength; i++) {
-                Number[i] = STxt[Numberindex];
-                Numberindex += 4;
-                dueDate[i] = Integer.parseInt(STxt[due_dayindex]);
-                due_dayindex += 4;
-                processingTime[i] = Integer.parseInt(STxt[processingTimeindex]);
-                processingTimeindex += 4;
-            };
+            //Total TxtLength 
+            setTxtLength((STxt.length / NumberOfColumns)-1);
+        }
     }
-}
     
-    public void output()
-    {
-      for(int i = 0 ; i < Number.length;i++)
+    private void SaveValueOfTxt(int TxtSize) {
+        if(TxtSize > 0 && TxtSize <= TxtLength)
+        {
+          //Set Size
+          int Numberindex = 4,due_dayindex = 5,processingTimeindex = 7;
+          Number = new String[TxtSize];
+          processingTime = new int[TxtSize];
+          dueDate = new int[TxtSize];
+          setReadTxtSize(TxtSize);
+
+          //save value of Txt
+          for (int i = 0; i < TxtSize; i++) {
+              Number[i] = STxt[Numberindex];
+              Numberindex += 4;
+              dueDate[i] = Integer.parseInt(STxt[due_dayindex]);
+              due_dayindex += 4;
+              processingTime[i] = Integer.parseInt(STxt[processingTimeindex]);
+              processingTimeindex += 4;
+          }
+        }else
+        {
+          System.out.print("Current Length is too long or too short.");
+        }
+    }
+    
+    public void output(){
+      if(ReadTxtSize > 0)
       {
-        System.out.println("Number : " + Number[i] + "\t");
-        System.out.println("processing time : " + processingTime[i] + "\t");
-        System.out.println("due-date : " + dueDate[i] + "\t");
+        for(int i = 0 ; i < Number.length;i++)
+        {
+          System.out.println("Number : " + Number[i] + "\t");
+          System.out.println("processing time : " + processingTime[i] + "\t");
+          System.out.println("due-date : " + dueDate[i] + "\t");
+        }
       }
     }
     
     public static void main(String[] args) throws IOException{
       {
         Readtxt RT = new Readtxt();
-        RT.setTxtData(".\\instances\\parallelMachine.txt");
+        RT.setReadTxtData(".\\instances\\parallelMachine.txt");
         RT.ReadTxt();
+        RT.SaveValueOfTxt(TxtLength);
         RT.output();
       }
+    }
+    
+    public int getReadTxtSize() {
+        return ReadTxtSize;
+    }
+    
+    public int getTxtLength() {
+        return TxtLength;
+    }
+    
+    public int[] getcompletionTime() {
+        return completionTime;
+    }
+
+    public int[] getSequence() {
+        return Sequence;
+    }
+
+    public int[] getProcessingTime() {
+        return processingTime;
+    }
+
+    public int[] getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int[] getDueDate() {
+        return dueDate;
+    }
+
+    public int[] getDeadline() {
+        return deadline;
+    }
+
+    public double[] getRevenue() {
+        return revenue;
+    }
+    
+    public String getTxtData()
+    {
+      return fileName;
     }
 }
